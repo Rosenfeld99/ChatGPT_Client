80bdee82840dc67b61106b19f18627625780123e
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-09-05 14:05:48 +0300

    creating home page with animations

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { useAuth } from '@clerk/clerk-react'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const DashboardLayout = () => {
+
+  const { userId, isLoaded } = useAuth();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoaded && !userId) {
+      navigate("/sign-in");
+    }
+  }, [isLoaded, userId, navigate]);
+
+  if (!isLoaded) return "Loading...";
   return (
-<div className="dashboardLayout">
+    <div className="dashboardLayout">
       <div className="menu">MENU</div>
       <div className="content">
         <Outlet />
       </div>
-    </div>  )
+    </div>)
 }
 
 export default DashboardLayout
