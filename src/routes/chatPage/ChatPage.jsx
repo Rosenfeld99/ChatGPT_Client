52d41cea33bd64ffba0aad584bc69e17a659fd68
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-09-08 00:10:10 +0300

    adding the chat page and ptompt with imagekit packages

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import "./chatPage.css";
+import NewPrompt from '../../components/newPrompt/NewPrompt';
 
 const ChatPage = () => {
+
+  const history = [
+    { message: "some text", role: "user" },
+    { message: "some text form ai", role: "" },
+    { message: "some text", role: "user" },
+    { message: "some text form ai", role: "" },
+    { message: "some text", role: "user" },
+    { message: "some text form ai", role: "" },
+    { message: "some text", role: "user" },
+    { message: "some text form ai", role: "" },
+    { message: "some text", role: "user" },
+    { message: "some text form ai", role: "" },
+    { message: "some text", role: "user" },
+    { message: "some text form ai", role: "" },
+  ]
+
   return (
-    <div>ChatPage</div>
-  )
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          {
+            history?.map((message, i) => (
+              <>
+                <div
+                  className={
+                    message.role === "user" ? "message user" : "message"
+                  }
+                  key={i}
+                >
+                  {message.message}
+                </div>
+
+              </>
+            ))}
+            <NewPrompt />
+        </div>
+      </div>
+    </div>)
 }
 
 export default ChatPage
